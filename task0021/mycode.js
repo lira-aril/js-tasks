c0ffb09e80f2382d67b09713f6ff30a728beb8f5
Lira Kakeeva <kakeeva.lira@gmail.com>
2021-12-18 22:06:02 +0600

    added task0021

---
+++ task0021/mycode.js
@@ -0,0 +1,72 @@
+let numbers_2 = []
+let numbers_3 = []
+
+let a = Number(prompt("Введите 1-е число:"))
+let b = Number(prompt("Введите 2-е число:"))
+let c = Number(prompt("Введите 3-е число:"))
+let d = Number(prompt("Введите 4-е число:"))
+let e = Number(prompt("Введите 5-е число:"))
+let f = Number(prompt("Введите 6-е число:"))
+let g = Number(prompt("Введите 7-е число:"))
+
+
+if ( a % 2 === 0 ) {
+    numbers_2.push(a)
+};
+
+if ( b % 2 === 0 ) {
+    numbers_2.push(b)
+};
+
+if ( c % 2 === 0 ) {
+    numbers_2.push(c)
+};
+
+if ( d % 2 === 0 ) {
+    numbers_2.push(d)
+};
+
+if ( e % 2 === 0 ) {
+    numbers_2.push(e)
+};
+
+if ( f % 2 === 0 ) {
+    numbers_2.push(f)
+};
+
+if ( g % 2 === 0 ) {
+    numbers_2.push(g)
+};
+console.log(numbers_2)
+
+
+if ( a % 3 === 0 ) {
+    numbers_3.push(a)
+};
+
+if ( b % 3 === 0 ) {
+    numbers_3.push(b)
+};
+
+if ( c % 3 === 0 ) {
+    numbers_3.push(c)
+};
+
+if ( d % 3 === 0 ) {
+    numbers_3.push(d)
+};
+
+if ( e % 3 === 0 ) {
+    numbers_3.push(e)
+};
+
+if ( f % 3 === 0 ) {
+    numbers_3.push(f)
+};
+
+if ( g % 3 === 0 ) {
+    numbers_3.push(g)
+};
+console.log(numbers_3)
+
+
