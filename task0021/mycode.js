50903c551a7545085d639779310df63d4b5ea118
Lira Kakeeva <kakeeva.lira@gmail.com>
2021-12-26 07:57:46 +0600

    added task0022

--- task0021/mycode.js
+++ task0021/mycode.js
@@ -1,13 +1,13 @@
-let numbers_2 = []
-let numbers_3 = []
+let numbers_2 = [];
+let numbers_3 = [];
 
-let a = Number(prompt("Введите 1-е число:"))
-let b = Number(prompt("Введите 2-е число:"))
-let c = Number(prompt("Введите 3-е число:"))
-let d = Number(prompt("Введите 4-е число:"))
-let e = Number(prompt("Введите 5-е число:"))
-let f = Number(prompt("Введите 6-е число:"))
-let g = Number(prompt("Введите 7-е число:"))
+let a = Number(prompt("Введите 1-е число:"));
+let b = Number(prompt("Введите 2-е число:"));
+let c = Number(prompt("Введите 3-е число:"));
+let d = Number(prompt("Введите 4-е число:"));
+let e = Number(prompt("Введите 5-е число:"));
+let f = Number(prompt("Введите 6-е число:"));
+let g = Number(prompt("Введите 7-е число:"));
 
 
 if ( a % 2 === 0 ) {
@@ -37,7 +37,8 @@ if ( f % 2 === 0 ) {
 if ( g % 2 === 0 ) {
     numbers_2.push(g)
 };
-console.log(numbers_2)
+
+console.log(numbers_2);
 
 
 if ( a % 3 === 0 ) {
@@ -67,6 +68,7 @@ if ( f % 3 === 0 ) {
 if ( g % 3 === 0 ) {
     numbers_3.push(g)
 };
-console.log(numbers_3)
+
+console.log(numbers_3);
 
 
---
+++ task0022/mycode.js
@@ -0,0 +1,14 @@
+let m = [];
+let n = [];
+
+for ( let i = 1; i <= 100; i++) {
+    if ( i % 3 === 0) {
+        m.push(i)
+    }
+    if ( i % 4 === 0){
+        n.push(i)
+    }
+};
+
+console.log(m);
+console.log(n);
