f60603106ac4c22adb30e00cfd4b238b7eb7e353
Lira Kakeeva <kakeeva.lira@gmail.com>
2022-03-10 15:25:29 +0600

    added task0019

---
+++ task0019/script.js
@@ -0,0 +1,8 @@
+let height_of_the_wall = Number(prompt("Сторона 1:"))
+let a = height_of_the_wall
+let width_of_the_wall = Number(prompt("Сторона 2:"))
+let b =  width_of_the_wall
+let area_of_the_wall = a * b
+let s = area_of_the_wall
+console.log(s)
+
